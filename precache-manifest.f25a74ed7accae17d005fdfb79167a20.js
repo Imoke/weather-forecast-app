27280ab60da3b5ac962e22ace2c4fdd0f7fd6ab6
lw <819740451@qq.com>
2020-02-02 13:59:49 +0800

    Initial the page of project

---
+++ precache-manifest.f25a74ed7accae17d005fdfb79167a20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54cb0b5f83aa45047e1d28973560440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f97fbdb5b9047342df6",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "9d7395df1adeab4f784a",
+    "url": "/static/css/main.ebfc6a2a.chunk.css"
+  },
+  {
+    "revision": "5f97fbdb5b9047342df6",
+    "url": "/static/js/2.9de3303b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.9de3303b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d7395df1adeab4f784a",
+    "url": "/static/js/main.061111f5.chunk.js"
+  },
+  {
+    "revision": "b5808d690746bf6896d1",
+    "url": "/static/js/runtime-main.e9155946.js"
+  },
+  {
+    "revision": "3fb1b4c4aaeb280c822404dc3e46e651",
+    "url": "/static/media/sunyu1.3fb1b4c4.png"
+  }
+]);
