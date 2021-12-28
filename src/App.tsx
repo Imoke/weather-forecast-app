5d48145044642c997c3f2d541ab871a5f2007140
lw <819740451@qq.com>
2021-12-28 22:43:33 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,22 @@ import './App.css';
 import Home from './pages/home'
 import Header from './pages/common/Header'
 import {CitysProvider} from './pages/common/Context/CityContext'
+import { BrowserRouter, Route} from 'react-router-dom';
 import Footer from './pages/common/Footer';
-import { Row, Col } from 'antd';
+// import { Row, Col } from 'antd';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <CitysProvider>
-        <Row>
-          <Header/>
-        </Row>
-        <Row type="flex" justify="center" align="middle" className="main-content">
-          <Col span={2}></Col>
-          <Col span={20}><Home/></Col>
-          <Col span={2}></Col>
-        </Row>
-        <Row>
-          <Footer/> 
-        </Row> 
+        <BrowserRouter>
+            <div>
+              <Header />
+              <Route path="/" exact component={Home}></Route>
+              <Route path="/about" exact component={Home}></Route>
+              <Footer/> 
+            </div>
+        </BrowserRouter>
       </CitysProvider>
     </div>
   );
