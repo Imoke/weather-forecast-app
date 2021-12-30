bfab65a2819344300feae5574403af7dcd8fd483
Liu Wang z003tx9r <wang.liu@siemens.com>
2021-12-30 20:26:34 +0800

    iframe

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,42 +4,45 @@ import Axios from 'axios';
 const About: React.FC = () => {
   // http://localhost:8765/ssologin?token=51935EEC40CED63A7E1742A8E88F0701&&username=1&&page=admin/blogs
   const [url, setUrl] = useState();
-  let api = `http://192.168.1.247:8765/ssologin?token=149CA1ACCECFE6B0A99E227CBF07455B&&username=1&&page=admin/blogs`;
   useEffect(() => {
+    let api = `http://192.168.1.247:8765/ssologin?token=593ED975B2EFAE69E6A3207D13F1540A&&username=1&&page=admin/blogs`;
     Axios({
       url: api,
       method: 'get',
       withCredentials: true,
     }).then((result: any) => {
-      const { success, url: iframeUrl } = result;
+      const { success, url: iframeUrl } = result.data;
+      window.console.log("url" + iframeUrl + ", url" + url)
       if (success) {
         setUrl(iframeUrl);
       }
     });
-  }, []);
-    return (
-        <div>
-        <AutoSizer disableHeight={true}>
-          {({ width }) => (
-            <iframe
-              title="title"
-              frameBorder="0"
-              // src='http://192.168.1.247:8765/admin/blog'
-              src={url}
-              scrolling="auto"
-              // onLoad={this.iframeOnLoad}
-              style={{
-                width: width,
-                height: '100vh',
-                position: 'relative',
-                overflow: 'hidden',
-                top: '0',
-              }}
-            />
-          )}
-        </AutoSizer>
-      </div>
-      );
+  }, [url]);
+
+  return (
+
+      <div>
+      <AutoSizer disableHeight={true}>
+        {({ width }) => (
+          <iframe
+            title="title"
+            frameBorder="0"
+            // src='http://192.168.1.247:8765/admin/blog'
+            src={url}
+            scrolling="auto"
+            // onLoad={hideSpinner}
+            style={{
+              width: width,
+              height: '100vh',
+              position: 'relative',
+              overflow: 'hidden',
+              top: '0',
+            }}
+          />
+        )}
+      </AutoSizer>
+    </div>
+    );
 }
 
 export default About
